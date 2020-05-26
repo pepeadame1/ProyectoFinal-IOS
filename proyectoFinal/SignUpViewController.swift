51a0ab8a099f41ca65979150eeb7e3acbc6fdba2
JulioRubio <julioarg97@gmail.com>
2020-05-26 17:52:42 -0500

    Se agregaron vistas
    Se agrego la vista de login y signup con sus constraints

---
+++ proyectoFinal/SignUpViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignUpViewController.swift
+//  proyectoFinal
+//
+//  Created by Vanessa Rubio on 26/05/20.
+//  Copyright © 2020 Jose Roberto. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
